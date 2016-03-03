4ea0de6dcc09a844edbec8ca7fe592faf9cdd5a6
brunobergot@gmail.com <>
2016-03-03 18:46:23 +0000

    On balance le squelette de satch basé sur le thème Lens de html5up

---
+++ lang/lens_fr.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'albums' => 'Albums photos',    
+'nombre_photo_un'   => '1 photo',
+'nombre_photo_plus' => '@nb@ photos',
+'voir_album' => "Voir l'album"
+    );
+?>
