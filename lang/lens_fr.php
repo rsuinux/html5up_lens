26ded281b961dade94ac5065bb4b3d7a74eec190
Remi Suinot <remi@suinot.org>
2020-05-20 12:55:34 +0200

    Mise en place d'un répertoire de version

--- lang/lens_fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'albums' => 'Albums photos',    
-'nombre_photo_un'   => '1 photo',
-'nombre_photo_plus' => '@nb@ photos',
-'voir_album' => "Voir l'album"
-	);
-?>
